abd34cac39f9c03bf881e5f58ed580a4bf32ede4
GabytaDev <gabyta.loyola@gmail.com>
2021-11-24 15:53:54 -0300

    borro comentarios inecesarios

--- index.js
+++ index.js
@@ -772,34 +772,6 @@ if(traerOperacionesDesdeLS("operaciones")=== null){
 
 
 
-// 
-/* const personas = [
-  { nombre: 'Edu', edad: 35 },
-  { nombre: 'Manuel', edad: 37 },
-  { nombre: 'Marta', edad: 42 },
-  { nombre: 'Edu', edad: 25 },
-];
-
-const busqueda = personas.reduce((acc, persona) => {
-  acc[persona.nombre] = ++acc[persona.nombre] || 0;
-  return acc;
-}, {});
-
-const duplicados = personas.filter( (persona) => {
-	return busqueda[persona.nombre];
-});
-
-console.log(duplicados);
-
-
-[
-  { edad: 35, nombre: "Edu" },
-  { edad: 25, nombre: "Edu" }
-]
-*/
-
-
-
 
 
 
